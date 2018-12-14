8ce051da698e80b81a60178ec5ec07d1570c6057
vizeke <vizeke@gmail.com>
2018-12-14 15:27:44 -0200

    feat(push): cria rota para notificação de multas

--- src/config/acesso-cidadao.js
+++ src/config/acesso-cidadao.js
@@ -1,8 +1,8 @@
 const _identityServerUrl = process.env.IDENTITY_SERVER_URL || 'https://acessocidadao.es.gov.br/is/';
-const _userInfoUrl = process.env.USER_INFO_URL || `${_identityServerUrl}connect/userinfo`;
+
 
 module.exports = {
     identityServerUrl: _identityServerUrl,
-    userInfoUrl: _userInfoUrl,
+    userInfoUrl: process.env.USER_INFO_URL || `${_identityServerUrl}connect/userinfo`,
     getDadosScope: process.env.GET_DADOS_SCOPE || 'SCOPE'
 };
